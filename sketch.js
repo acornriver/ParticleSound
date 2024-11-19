146c279805d21098d37a7ac747ca862fa6a7fe50
acornriver <kmicky0806@gmail.com>
2024-11-19 15:18:14 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+//by sb prof
+
+let particleSystem;
+
+function setup() {
+  createCanvas(300, 600);
+  particleSystem = new ParticleSystem(createVector(width / 2, 50));
+}
+
+function draw() {
+  background(51);
+  particleSystem.run();
+}
+
+function mouseDragged() {
+  particleSystem.origin = createVector(mouseX, mouseY);
+  particleSystem.addParticle();
+}
